0947a3e00ea87776d56f9de91ce30b79e29fede9
Fabio Arthur Ebsi von Schelling Goldman <f.vonschellinggoldman@campus.tu-berlin.de>
2019-04-04 15:30:59 -0400

    Improved code base, by splitting into more functions

--- main.py
+++ main.py
@@ -1,36 +1,56 @@
 '''
+Author: Fabio von Schelling Goldman
 TODO:
 - Figure out how to make more adaptable and versatile
---> Don't double playlists
 --> Read in different types of files
 '''
+
 from trello import TrelloClient, os
 import spotipy
 import spotipy.util as util
 from dotenv import load_dotenv
 
 
-def spotify(category):
+def setup_spotify(username):
     scope = "playlist-modify-public"
-    username = os.getenv("SPOTIFYUSERNAME")
     token = util.prompt_for_user_token(username, scope, client_id=os.getenv("SPOTIFYID"),
                                        client_secret=os.getenv("SPOTIFYSECRET"),
                                        redirect_uri="http://localhost:8888/callback/")
     if token:
         sp = spotipy.Spotify(auth=token)
         sp.trace = False
+        return sp
     else:
         print("Can't get token for", username)
         exit(-1)
-    playlists = sp.user_playlist_create(username, category.name)
+
+def new_list(username, sp, new_name):
+    existing_lists = sp.user_playlists(username)['items']
+    for name in existing_lists:
+        if new_name == name['name']:
+            print("Playlist with this name exists already, please choose a new name.")
+            return False
+    return True
+
+
+def add_all_cards(username, sp, new_playlist, category):
     for card in category.list_cards():
         result = sp.search(card.name, type="artist")
         if len(result['artists']['items']) == 0:
             continue
         tops = sp.artist_top_tracks(result['artists']['items'][0]['id'])
         for track in tops['tracks']:
-            sp.user_playlist_add_tracks(username, playlists['id'], [track['uri']])
+            sp.user_playlist_add_tracks(username, new_playlist['id'], [track['uri']])
             print(track['name'])
+
+
+def spotify(category):
+    username = os.getenv("SPOTIFYUSERNAME")
+    sp = setup_spotify(username)
+    if not new_list(username, sp, category.name):
+        return
+    new_playlist = sp.user_playlist_create(username, category.name)
+    add_all_cards(username, sp, new_playlist, category)
     print("Finished processing a list")
 
 
